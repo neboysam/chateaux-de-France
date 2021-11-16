aef58b30e550dc42f79fd966d2a479ae66389376
neboysam <monte.nemi@gmail.com>
2021-11-16 11:58:39 +0100

    added some of the CRUD functionalities

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const path = require('path');
 const methodOverride = require('method-override');
+const Castle = require('./models/castle');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/castles', {
   useNewUrlParser: true,
@@ -17,16 +25,39 @@ db.once('open', () => {
   console.log('Database connected.');
 });
 
-const app = express();
-
+//main page
 app.get('/', (req, res) => {
-  res.send('Welcome to the Homepage!');
+  res.render('./home');
 });
 
-app.get('/castles', (req, res) => {
-  res.send('Main page');
+//homepage with all castles
+app.get('/castles', async (req, res) => {
+  const castles = await Castle.find({});
+  res.render('castles/index', { castles });
 });
+
+//form to add new castle
+app.get('/castles/new', (req, res) => {
+  res.render('castles/new');
+});
+
+//add new castle into db
+app.post('/castles', async (req, res) => {
+  const { castle } = req.body;
+  const newCastle = new Castle(castle);
+  await newCastle.save();
+  res.redirect('/castles');
+});
+
+//show a castle
+app.get('/castles/:id', async (req, res) => {
+  const { id } = req.params;
+  const castle = await Castle.findById(id);
+  res.render('castles/show', { castle });
+});
+
+
 
 app.listen(3000, () => {
-  console.log("Server is started on port 3000");
+  console.log("Castles: Server is started on port 3000");
 })
