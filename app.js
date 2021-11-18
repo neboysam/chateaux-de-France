722ac63bf1a17a1b0bd3874b545ecb21b7b4864f
neboysam <monte.nemi@gmail.com>
2021-11-18 19:03:27 +0100

    added error handling with joi

--- app.js
+++ app.js
@@ -4,9 +4,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const path = require('path');
 const methodOverride = require('method-override');
 const Castle = require('./models/castle');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -31,10 +34,10 @@ app.get('/', (req, res) => {
 });
 
 //homepage with all castles
-app.get('/castles', async (req, res) => {
+app.get('/castles', catchAsync(async (req, res) => {
   const castles = await Castle.find({});
   res.render('castles/index', { castles });
-});
+}));
 
 //form to add new castle
 app.get('/castles/new', (req, res) => {
@@ -42,42 +45,69 @@ app.get('/castles/new', (req, res) => {
 });
 
 //add new castle into db
-app.post('/castles', async (req, res) => {
+app.post('/castles', catchAsync(async (req, res, next) => {
   const { castle } = req.body;
+  /* if(!castle) throw new ExpressError(400, 'Invalid Castle Data'); */ //all the form fields are empty = !req.body.castle
+  const castleSchema = Joi.object({
+    castle: Joi.object({
+      title: Joi.string().required(),
+      location: Joi.string().required(),
+      image: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      description: Joi.string().required()
+    }).required() 
+  });
+  /* const result = castleSchema.validate(req.body); */
+  const { error } = castleSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(400, msg);
+  }
+  //console.log(result);
   const newCastle = new Castle(castle);
   await newCastle.save();
   res.redirect('/castles');
-});
+}));
 
 //show castle
-app.get('/castles/:id', async (req, res) => {
+app.get('/castles/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const castle = await Castle.findById(id);
   res.render('castles/show', { castle });
-});
+}));
 
 //form to update castle
-app.get('/castles/:id/edit', async (req, res) => {
+app.get('/castles/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params;
   const castle = await Castle.findById(id);
   res.render('./castles/edit', { castle });
-});
+}));
 
 //update castle
-app.put('/castles/:id', async (req, res) => {
+app.put('/castles/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const { castle } = req.body;
   const newCastle = await Castle.findByIdAndUpdate(id, castle);
   await newCastle.save();
   res.redirect(`/castles/${newCastle._id}`);
-});
+}));
 
 //delete castle
-app.delete('/castles/:id', async (req, res) => {
+app.delete('/castles/:id', catchAsync(async (req, res, next) => {
   const { id } = req.params;
-  await Castle.deleteOne({id});
-  //await Castle.findByIdAndDelete(id);
+  await Castle.findByIdAndDelete(id);
   res.redirect('/castles');
+}));
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError(404, 'Page not found.'));
+});
+
+app.use((err, req, res, next) => {
+  //const { statusCode = 500, message = 'Something went wrong'} = err;
+  const { statusCode = 500 } = err;
+  if(!err.message) err.message = 'Something went wrong';
+  res.status(statusCode).render('./castles/error', { err });
 });
 
 app.listen(3000, () => {
