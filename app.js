132b77fed1ddba7af4fad10f8dc03d9f037ed049
neboysam <monte.nemi@gmail.com>
2021-11-28 22:10:13 +0100

    update to deploy

--- app.js
+++ app.js
@@ -27,12 +27,13 @@ const MongoStore = require('connect-mongo');
 
 //mongoDB config
 //'mongodb://localhost:27017/castles'
+//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  /* useCreateIndex: true,
+  useFindAndModify: false */
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
