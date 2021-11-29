b1db8737e742cffe2ed26df4547b08a1e1b2e411
neboysam <monte.nemi@gmail.com>
2021-11-29 17:05:24 +0100

    update to deploy

--- app.js
+++ app.js
@@ -1,77 +1,83 @@
-
+/* CATALOGUE OF THE CASTLES IN FRANCE  */
+/* =================================== */
 
 if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
 
 const express = require('express');
-const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
+const Joi = require('joi');
+const path = require('path');
 const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const Castle = require('./models/castle');
+const Review = require('./models/review');
 const User = require('./models/user');
-const userRoutes = require('./routes/users');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const castleRoutes = require('./routes/castles');
 const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const MongoStore = require('connect-mongo');
 
-const MongoDBStore = require("connect-mongo")(session);
-
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
-
+//mongoDB config
+//process.env.DB_URL 
+const dbUrl = 'mongodb://localhost:27017/castles';
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  /* useCreateIndex: true,
+  useFindAndModify: false */
 });
-
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected.');
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+/* app.use(express.static('public')); */
+app.use(express.static(path.join(__dirname, 'public')));
 
-const store = new MongoDBStore({
-  url: dbUrl,
-  secret,
-  touchAfter: 24 * 60 * 60
+const secret = process.env.SECRET || 'thisisasecret';
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret //or, secret: 'thisisasecret'
+  }
 });
 
-store.on("error", function (e) {
-  console.log("SESSION STORE ERROR", e)
-})
+store.on('error', function(e) {
+  console.log("Session store error: ", e);
+});
 
 const sessionConfig = {
-  store,
+  store, //store: store
   name: 'session',
   secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
-      httpOnly: true,
-      // secure: true,
-      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-      maxAge: 1000 * 60 * 60 * 24 * 7
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }
-
 app.use(session(sessionConfig));
 app.use(flash());
 
@@ -82,32 +88,46 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//flash middleware
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
+  //console.log(session);
   next();
-})
-
-app.use('/', userRoutes);
-app.use('/castles', castleRoutes)
-app.use('/castles/:id/reviews', reviewRoutes)
-
-app.get('/', (req, res) => {
-  res.render('home')
 });
 
-app.all('*', (req, res, next) => {
-  next(new ExpressError('Page Not Found', 404))
-})
+//routes middleware
+app.use('/castles', castleRoutes);
+app.use('/castles/:id/review', reviewRoutes);
+app.use('/', userRoutes);
 
+/* app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'mail@gmail.com', username: 'colt' });
+  const newUser = await User.register(user, 'password');
+  res.send(newUser);
+}); */
+
+//main page
+app.get('/', (req, res) => {
+  res.render('./home');
+});
+
+//if page is not found
+app.all('*', (req, res, next) => {
+  next(new ExpressError(404, 'Page not found.'));
+});
+
+//error handling middleware
 app.use((err, req, res, next) => {
+  //const { statusCode = 500, message = 'Something went wrong'} = err;
   const { statusCode = 500 } = err;
-  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-  res.status(statusCode).render('error', { err })
-})
+  if(!err.message) err.message = 'Something went wrong';
+  res.status(statusCode).render('error', { err });
+});
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Serving on port ${port}`)
+  console.log(`Castles: Server is started on port ${port}`);
 })
