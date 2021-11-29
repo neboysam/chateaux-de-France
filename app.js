6bc200f1f6732f26baa9630f66320419df7347b8
neboysam <monte.nemi@gmail.com>
2021-11-29 15:41:26 +0100

    update to deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* CATALOGUE OF THE CASTLES IN FRANCE  */
 /* =================================== */
 
-if (process.env.NODE_ENV !== "production") {
+/* if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
 
@@ -26,8 +26,6 @@ const LocalStrategy = require('passport-local');
 const MongoStore = require('connect-mongo');
 
 //mongoDB config
-//'mongodb://localhost:27017/castles'
-//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
@@ -51,7 +49,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 /* app.use(express.static('public')); */
-app.use(express.static(path.join(__dirname, 'public')));
+/*app.use(express.static(path.join(__dirname, 'public')));
 
 const secret = process.env.SECRET || 'thisisasecret';
 
@@ -60,7 +58,7 @@ const store = MongoStore.create({
   touchAfter: 24 * 60 * 60,
   crypto: {
     secret
-}
+  }
 });
 
 store.on('error', function(e) {
@@ -109,7 +107,7 @@ app.use('/', userRoutes);
 }); */
 
 //main page
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.render('./home');
 });
 
@@ -130,4 +128,116 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Castles: Server is started on port ${port}`);
+}) */
+
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+  store,
+  name: 'session',
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+  res.status(statusCode).render('error', { err })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`)
 })
