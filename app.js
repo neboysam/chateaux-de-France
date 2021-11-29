75fcc710ca6b751797f4c311914b9d85b9945f21
neboysam <monte.nemi@gmail.com>
2021-11-29 17:09:46 +0100

    update to deploy

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ const LocalStrategy = require('passport-local');
 const MongoStore = require('connect-mongo');
 
 //mongoDB config
-//process.env.DB_URL 
-const dbUrl = 'mongodb://localhost:27017/castles';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/castles';
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
